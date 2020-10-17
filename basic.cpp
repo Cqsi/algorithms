3cd00babd3b9e46456b784f0d8fc254a79749900
Casimir Rönnlöf <casimirr04@gmail.com>
2020-10-17 10:15:12 +0300

    test2

---
+++ basic.cpp
@@ -0,0 +1,21 @@
+// In this file you can find a lot of basic competitive programming stuff in C++, I made it because I forget things easily
+
+using namespace std; // you don't have to type out "std" each time
+
+// libraries to inlcude
+#include <iostream> // input and output
+#include <algorithm> // a ton of useful algorithms
+
+#define ll long long // just type "ll" instead of "long long"
+#define ar array // ar instead of array
+
+int main() {
+    int num; 
+
+    cin >> num; // input
+    cout << "Hello World!"; // output
+
+
+
+    return 0; // end of program
+}
--- check.cpp
+++
@@ -1,9 +0,0 @@
-// This file is here so you can check that your c++ compiler works (and similar problems)
-using namespace std;
-#include <iostream>
-
-int main() {
-    int num = 0;
-    cout << "Hello World!";
-    return 0;
-}
