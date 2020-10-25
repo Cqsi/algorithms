1d1420eef920cb315affe293f5e8f829c3948c1a
Casimir Rönnlöf <casimirr04@gmail.com>
2020-10-25 21:21:19 +0200

    testing

--- definitions.cpp
+++ definitions.cpp
@@ -7,4 +7,18 @@
 #define seti(v) set<int> uniques(v.begin(), v.end())
 #define printv(v) for(auto i : v){cout << i;}
 
-// 
+#define gcd(a,b) __gcd(a,b)
+
+template<class T> inline vector<pair<T,int> > factorize(T n)//NOTES:factorize(
+  {vector<pair<T,int> > R;for (T i=2;n>1;){if (n%i==0){int C=0;for (;n%i==0;C++,n/=i);R.push_back(make_pair(i,C));}
+   i++;if (i>n/i) i=n;}if (n>1) R.push_back(make_pair(n,1));return R;}
+
+template<class T> inline vector<T> primefactor(T n){
+    vector<T> factors;
+    while(n%2==0){
+        factors.push_back
+    }
+}
+
+// CONSTANTS
+const double PI=acos(-1.0);
