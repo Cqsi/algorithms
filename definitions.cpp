3a60047703e60191ac1366b6c3a714e857e9e7ed
Casimir Rönnlöf <casimirr04@gmail.com>
2020-10-25 07:28:21 +0200

    definitions

---
+++ definitions.cpp
@@ -0,0 +1,7 @@
+// a collection of definitions that *might* be useful in competitive programming
+// some of them might destroy the logic and readability in the program so idk
+
+#define ll long long 
+#define size(arr) sizeof(arr)/sizeof(arr[0])
+#define seti(v) set<int> uniques(v.begin(), v.end())
+#define printv(v) for(auto i : v){cout << i;}
---
+++ search & other/maximumsubarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int n_3(int arr[]){
+    int best = 0;
+    int n = sizeof(arr)/sizeof(arr[0])
+
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            int s = 0;
+            for(int k = )
+        }
+    }
+
+
+    return best;
+}
+
+int main(){
+
+    int arr[] = {-1, 2, 4, -3, 5, 2, -5, 2};
+
+    return 0;
+}
