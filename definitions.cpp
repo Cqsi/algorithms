aadd0ba93f89b6f7a6bdd3a94fe4cb392e9356a0
Casimir Rönnlöf <casimirr04@gmail.com>
2020-10-25 20:46:43 +0200

    edit

--- definitions.cpp
+++ definitions.cpp
@@ -1,7 +1,10 @@
 // a collection of definitions that *might* be useful in competitive programming
 // some of them might destroy the logic and readability in the program so idk
+// https://codeforces.com/contest/8/submission/36760
 
 #define ll long long 
 #define size(arr) sizeof(arr)/sizeof(arr[0])
 #define seti(v) set<int> uniques(v.begin(), v.end())
 #define printv(v) for(auto i : v){cout << i;}
+
+// 
--- search & other/maximumsubarray.cpp
+++ search & other/maximumsubarray.cpp
@@ -3,24 +3,64 @@ using namespace std;
 
 #define ll long long
 
-int n_3(int arr[]){
+
+// Very naive solution, O(n^3)
+int n_3(vector<int> v){
     int best = 0;
-    int n = sizeof(arr)/sizeof(arr[0])
+    int n = v.size();
 
     for(int i = 0; i < n; i++){
         for(int j = i; j < n; j++){
             int s = 0;
-            for(int k = )
+            for(int k = i; k < j+1; k++){
+                s+=v[k];
+            }
+            best = max(best, s);
         }
     }
-
 
     return best;
 }
 
+
+// Efficient solution, O(n^2)
+int n_2(vector<int> v){
+    int best = 0;
+    int n = v.size();
+
+    for(int i = 0; i < n; i++){
+        int s = 0;
+        for(int j = i; j < n; j++){
+            s+=v[j];
+            best = max(best, s);
+        }
+    }
+
+    return best;
+}
+
+
+// Best solution, O(n)
+int n_1(vector<int> v){
+    int best = 0;
+    int n = v.size();
+    int s = 0;
+
+    for (int i = 0; i < n; i++){
+        s = max(v[i], s+v[i]);
+        best = max(best, s);
+    }
+    
+
+    return best;
+}
+
+
+
 int main(){
 
-    int arr[] = {-1, 2, 4, -3, 5, 2, -5, 2};
+    vector<int> v = {-1, 2, 4, -3, 5, 2, -5, 2};
 
+    cout << n_1(v);
     return 0;
 }
