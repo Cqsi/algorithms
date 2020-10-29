133162d52c50aaf832ee85a75e044659053c07a7
Casimir Rönnlöf <casimirr04@gmail.com>
2020-10-29 20:25:43 +0200

    test

---
+++ search & other/twopointer.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+ 
+// Two pointer technique based solution to find
+// if there is a pair in A[0..N-1] with a given sum.
+int isPairSum(int A[], int N, int X)
+{
+    // represents first pointer
+    int i = 0;
+ 
+    // represents second pointer
+    int j = N - 1;
+ 
+    while (i < j) {
+ 
+        // If we find a pair
+        if (A[i] + A[j] == X)
+            return 1;
+ 
+        // If sum of elements at current
+        // pointers is less, we move towards
+        // higher values by doing i++
+        else if (A[i] + A[j] < X)
+            i++;
+ 
+        // If sum of elements at current
+        // pointers is more, we move towards
+        // lower values by doing j--
+        else
+            j--;
+    }
+    return 0;
+}
+
+int main()
+{
+    int arr[] = { 3, 5, 9, 2, 8, 10, 11 };
+    int val = 17;
+    int arrSize = *(&arr + 1) - arr;
+
+    cout << (bool)isPairSum(arr, arrSize, val);
+ 
+    return 0;
+}
