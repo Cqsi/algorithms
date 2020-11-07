1e697063530c1ae082a785fe34126e84d8b1ff0f
Casimir Rönnlöf <casimirr04@gmail.com>
2020-11-07 17:28:43 +0200

    changed error

--- eightqueens.cpp
+++ eightqueens.cpp
@@ -14,7 +14,7 @@ void search(int y) {
         count++;
         return;
     }   
-    for (int x = 0; x < y+1; x++) {
+    for (int x = 0; x < n; x++) {
         if (column[x] || diag1[x+y] || diag2[x-y+n-1]) continue;
         column[x] = diag1[x+y] = diag2[x-y+n-1] = 1;
         search(y+1);
